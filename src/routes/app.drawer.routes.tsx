f1d18b99c1ab06d170c3e142d0d6a6723e9799a7
Jefferson Charlle <contato@jefferdeveloper.com>
2021-10-01 10:41:34 -0300

    add: theme styles drawer

--- src/routes/app.drawer.routes.tsx
+++ src/routes/app.drawer.routes.tsx
@@ -6,10 +6,24 @@ const {Navigator, Screen} = createDrawerNavigator();
 import { Movies } from "../pages/Movies";
 
 import { AppStackRoutes } from './app.stack.routes';
+import { useTheme } from "styled-components";
 
 export const AppDrawerRoutes = () => {
+    const theme = useTheme();
+
+
     return (
         <Navigator 
+            screenOptions={{
+                headerShown:false, //remover header
+                drawerStyle:{
+                    backgroundColor: theme.colors.drawer,
+                    paddingTop: 20,
+                },
+                drawerActiveBackgroundColor:theme.colors.danger,
+                drawerActiveTintColor: theme.colors.shape,
+                drawerInactiveTintColor:theme.colors.text,
+            }}
             
         >
             <Screen 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,12 @@
 export default {
     colors:{
         primary:'#5363d3',
+        drawer:'#121212',
         secondary:'#ff872c',
         secondary_light: 'rgba(255,135,44,0.3)',
         success:'#12A454',
         success_light:'rgba(18,164,84,0.5)',
-        danger:'#E83F53',
+        danger:'#E72f49',
         danger_light: 'rgba(232,63,91,0.5)',
         shape:'#FFFFFF',
         title:'#363f5f',
