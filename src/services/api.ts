76c22dcb23697facd4b0bb1348e8c2162bac1332
Jefferson Charlle <contato@jefferdeveloper.com>
2021-10-01 23:17:54 -0300

    add: create services api

---
+++ src/services/api.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+/**============================================
+ *URL FILMES EM CARTAZ          TODO HEADER
+ *https://api.themoviedb.org/3/movie/now_playing?api_key=API&language=pt-BR&page=1
+ *=============================================**/
+
+const key = '660709e57868ca59f65c23440f729164';
+
+const api = axios.create({
+    baseURL:'https://api.themoviedb.org/3'
+});
+
+export {api, key}
