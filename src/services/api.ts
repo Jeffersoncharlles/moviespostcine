808c9d158b693a94a1f5a1e997c36b7a23f36ee1
jeffersoncharlles <contato@jefferdeveloper.com>
2021-10-10 03:24:04 -0300

    add: env config

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+const {KEY_API} = process.env;
+
 /**============================================
  *URL FILMES EM CARTAZ          TODO HEADER
  *https://api.themoviedb.org/3/movie/now_playing?api_key=API&language=pt-BR&page=1
  *=============================================**/
 
-const key = '660709e57868ca59f65c23440f729164';
+const key = KEY_API;
 const posterPath = 'https://image.tmdb.org/t/p/original';
 const posterPath2 = 'https://image.tmdb.org/t/p/w500';
 
